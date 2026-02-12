82da782f5ee834b4eb53de82e73aa21dd35df702
Andreas Schubert <andy.schubert@outlook.com>
2026-02-12 14:48:38 +0100

    loadgen: add HdrHistogram for latency percentiles

--- loadgen/build.gradle.kts
+++ loadgen/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     implementation("io.grpc:grpc-netty-shaded:1.60.0")
     implementation("io.grpc:grpc-protobuf:1.60.0")
     implementation("io.grpc:grpc-stub:1.60.0")
+    implementation("org.hdrhistogram:HdrHistogram:2.1.12")
     compileOnly("org.apache.tomcat:annotations-api:6.0.53")
 }
 
